3c1fcbbd72d3c9917bad66995f0582fc67ffe5fd
Vasyl <145612678+MoisieievVasya@users.noreply.github.com>
2025-06-25 00:21:43 +0300

    changed db for postgresql

--- config.py
+++ config.py
@@ -3,4 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DB_USER_NAME = os.getenv("DB_USER_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
